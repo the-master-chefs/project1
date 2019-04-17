c628a9920bd426d74f4f87f47c22f334bd650de3
owaisj <jamil.owais@gmail.com>
2019-04-17 17:41:18 -0500

    Added getRecipe function

--- app.js
+++ app.js
@@ -2,6 +2,35 @@ var cabinet = [];
 var food = [];
 $(document).ready(function(){
 
+//AJAX Request Function
+let getRecipe = (meal) => {
+    let queryURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + meal;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then((response) => {
+        console.log(response);  
+        //Build an array of the required ingredients
+        let ingredients = [];
+        for (let i = 1; i < 20; i++) {
+            let currentIngredient = response.meals[0]['strIngredient' + i];
+            if (currentIngredient !== "") {
+                ingredients.push(currentIngredient);
+            } else {
+                break;
+            }
+        }
+        //Display Ingredients
+        console.log(ingredients);
+        //Display Recipe
+        console.log(response.meals[0].strInstructions);
+    })
+}    
+
+
+
+
 //collecting ingredients for master list
 $("#salt").on("click", function (e) {
     e.preventDefault();           
